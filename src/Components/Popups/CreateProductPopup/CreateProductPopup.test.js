5098afd0f4eeaa5e988c7760ed4fcdaddbdf5d8e
David Lamberson <dlamberson@rise8.us>
2021-04-02 12:34:37 -0400

    add tags

--- src/Components/Popups/CreateProductPopup/CreateProductPopup.test.js
+++ src/Components/Popups/CreateProductPopup/CreateProductPopup.test.js
@@ -8,6 +8,19 @@ describe('<CreateProductPopup />', () => {
 
     const closePopupMock = useModuleMock('Redux/Popups/actions', 'closePopup')
     const submitProductMock = useModuleMock('Redux/Products/actions', 'requestCreateProduct')
+    const selectAllTagsMock = useModuleMock('Redux/Tags/selectors', 'selectAllTags')
+
+    const returnedTags = [
+        { id: 1, label: 'Tag 1', description: '', color: '#000000' },
+        { id: 2, label: 'Tag 2', description: '', color: '#000000' },
+        { id: 13, label: 'scoped::label 1', description: '', color: '#000000' },
+        { id: 14, label: 'scoped::label 2', description: '', color: '#000000' }
+    ]
+
+    beforeEach(() => {
+        useDispatchMock().mockReturnValue({})
+        selectAllTagsMock.mockReturnValue(returnedTags)
+    })
 
     test('should render properly', () => {
         render(<CreateProductPopup />)
@@ -18,8 +31,7 @@ describe('<CreateProductPopup />', () => {
         expect(screen.getByTestId('CreateProductPopup__input-gitlabProjectId')).toBeInTheDocument()
     })
 
-    test('should execute onSubmit', () => {
-        useDispatchMock().mockReturnValue({})
+    test('should execute onSubmit', async() => {
         render(<CreateProductPopup />)
 
         const name = 'My New Product'
@@ -36,14 +48,54 @@ describe('<CreateProductPopup />', () => {
         userEvent.type(nameInput, name)
         userEvent.type(gitlabProjectIdInput, gitlabProjectId)
         userEvent.type(descriptionInput, description)
+
+        fireEvent.click(await screen.findByTitle('Open'))
+        const option = screen.getByText('Tag 2')
+        expect(option).toBeInTheDocument()
+        fireEvent.click(option)
+
         fireEvent.click(screen.getByText('Submit'))
 
         expect(submitProductMock).toHaveBeenCalledTimes(1)
-        expect(submitProductMock.mock.calls[0][0]).toEqual({ name, gitlabProjectId, description })
+        expect(submitProductMock.mock.calls[0][0]).toEqual({ name, gitlabProjectId, description, tagIds: [2] })
+    })
+    test('should add & remove tag', async() => {
+        render(<CreateProductPopup id = {4}/>)
+        fireEvent.click(await screen.findByTitle('Open'))
+
+        // Add tag
+        const option = screen.getByText('Tag 2')
+        expect(option).toBeInTheDocument()
+        fireEvent.click(option)
+
+        expect(await screen.getByText('Tag 2')).toBeInTheDocument()
+
+        // Remove tag
+        fireEvent.click(await screen.findByTitle('Clear'))
+        expect(await screen.queryByText('Tag 2')).not.toBeInTheDocument()
     })
 
+    test('should allow only one scoped tag', async() => {
+        render(<CreateProductPopup id = {4}/>)
+
+        // add first scoped label
+        fireEvent.click(await screen.findByTitle('Open'))
+        const option = screen.getByText('scoped::label 1')
+        expect(option).toBeInTheDocument()
+        fireEvent.click(option)
+
+        expect(await screen.findByText('label 1')).toBeInTheDocument()
+
+        // add second scoped label
+        fireEvent.click(await screen.findByTitle('Open'))
+        const option2 = screen.getByText('scoped::label 2')
+        expect(option2).toBeInTheDocument()
+        fireEvent.click(option2)
+
+        expect(await screen.queryByText('label 1')).not.toBeInTheDocument()
+        expect(await screen.findByText('label 2')).toBeInTheDocument()
+    })
     test('should close popup', () => {
-        useDispatchMock().mockReturnValue({})
         render(<CreateProductPopup />)
 
         fireEvent.click(screen.getByTestId('Popup__button-close'))
@@ -56,7 +108,8 @@ describe('<CreateProductPopup />', () => {
             errors: {
                 'products/createOne': [
                     'name error',
-                    'Gitlab error'
+                    'Gitlab error',
+                    'Tag error'
                 ]
             }
         }
@@ -64,6 +117,7 @@ describe('<CreateProductPopup />', () => {
 
         expect(screen.getByText('name error')).toBeInTheDocument()
         expect(screen.getByText('Gitlab error')).toBeInTheDocument()
+        expect(screen.getByText('Tag error')).toBeInTheDocument()
 
     })
 
