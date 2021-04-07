9f4febf0df81689961a451def4c6852ec0e14a68
David Lamberson <dlamberson@rise8.us>
2021-04-07 18:13:18 +0000

    Resolve "Add Tag Component to Admin Page"

---
+++ src/Components/Admin/TagsTab/TagsTab.js
@@ -0,0 +1,88 @@
+import { Box, Button, IconButton, makeStyles, Typography } from '@material-ui/core'
+import { Add, Delete, Edit } from '@material-ui/icons'
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { openPopup } from '../../../Redux/Popups/actions'
+import { requestDeleteTag } from '../../../Redux/Tags/actions'
+import TagConstants from '../../../Redux/Tags/constants'
+import { selectAllTags } from '../../../Redux/Tags/selectors'
+import { Table } from '../../Table'
+import { Tag } from '../../Tag'
+
+const useStyles = makeStyles(theme => ({
+    button: {
+        '&:hover': {
+            color: theme.palette.primary.main
+        },
+        height: 40
+    }
+}))
+
+function TagsTab() {
+    const classes = useStyles()
+    const dispatch = useDispatch()
+    const allTags = useSelector(selectAllTags)
+
+    const createTag = () => dispatch(openPopup(TagConstants.CREATE_TAG, 'CreateTagPopup'))
+
+    const buildRows = () => {
+        return allTags.map(tag => [buildTag(tag), tag.description, buildColor(tag.color), buildActions(tag.id)])
+    }
+
+    const buildTag = (tag) => {
+        return (
+            <Box display = 'flex'>
+                <Tag {...tag}/>
+            </Box>
+        )
+    }
+
+    const buildColor = (color) => {
+        return (
+            <Typography variant = 'body2' style = {{ color }}>{color}</Typography>
+        )
+    }
+
+    const buildActions = (id) => {
+
+        return (
+            <>
+                <IconButton
+                    title = 'edit'
+                    color = 'secondary'
+                    onClick = {() => dispatch(openPopup(TagConstants.UPDATE_TAG, 'UpdateTagPopup', { id }))}
+                >
+                    <Edit />
+                </IconButton>
+                <IconButton
+                    title = 'delete'
+                    color = 'secondary'
+                    onClick = {() => dispatch(requestDeleteTag(id))}
+                >
+                    <Delete />
+                </IconButton>
+            </>
+        )
+    }
+
+    return (
+        <div style = {{ padding: '24px' }}>
+            <Box display = 'block' width = '75vw' margin = 'auto' textAlign = 'right' padding = '24px 0'>
+                <Button
+                    variant = 'text'
+                    startIcon = {<Add/>}
+                    className = {classes.button}
+                    onClick = {createTag}
+                >
+                    Add New Tag
+                </Button>
+            </Box>
+            <Table
+                rows = {buildRows()}
+                columns = {['Tag', 'Description', 'Color', '']}
+            />
+        </div>
+    )
+}
+
+export default TagsTab
---
+++ src/Components/Table/Table.js
@@ -0,0 +1,88 @@
+import { makeStyles, Paper, Table as MUITable, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core'
+import PropTypes from 'prop-types'
+import React from 'react'
+
+// NOTE: reference - MUI Sticky Header Table -- https://codesandbox.io/s/x17ef?file=/demo.js:1069-1212
+// Can handle pagination if/when it needs to be implemented.
+//Table container and maxheight allow for scrolling on table instead of page
+
+const useStyles = makeStyles(theme => ({
+    actions: {
+        padding: '0'
+    },
+    regular: {
+        maxWidth: 'auto',
+        padding: '0 16px',
+        color: theme.palette.text.secondary
+    }
+}))
+
+function Table({ tableWidth, align, stickyHeader, rows, columns, transparent }) {
+    const classes = useStyles()
+
+    const end = align === 'left' ? 'right' : 'left'
+
+    const getAlign = (columnsLength, currentIndex) => columnsLength - 1 !== currentIndex ? align : end
+
+    return (
+        <Paper
+            data-testid = 'Table__paper'
+            style = {{
+                width: tableWidth,
+                margin: 'auto',
+                backgroundColor: `${transparent ? 'transparent' : 'default'}`
+            }}
+        >
+            <MUITable { ...stickyHeader }>
+                <TableHead>
+                    <TableRow>
+                        {columns.map((column, index) => (
+                            <TableCell
+                                key = {index}
+                                align = {getAlign(columns.length, index)}
+                            >
+                                {column}
+                            </TableCell>
+                        ))}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row, index) => {
+                        return (
+                            <TableRow tabIndex = {-1} key = {index} hover >
+                                {columns.map((column, idx) => (
+                                    <TableCell
+                                        key = {`${index}-${idx}`}
+                                        align = {getAlign(columns.length, idx)}
+                                        className = {column.length === 0 ? classes.actions : classes.regular}
+                                    >
+                                        {row[idx]}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        )
+                    })}
+                </TableBody>
+            </MUITable>
+        </Paper>
+    )
+}
+
+Table.propTypes = {
+    columns: PropTypes.arrayOf(PropTypes.string).isRequired,
+    rows: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.node, PropTypes.string])).isRequired,
+    stickyHeader: PropTypes.bool,
+    tableWidth: PropTypes.string,
+    align: PropTypes.string,
+    transparent: PropTypes.bool
+}
+
+Table.defaultProps = {
+    stickyHeader: true,
+    maxHeight: '440px',
+    align: 'left',
+    tableWidth: '75vw',
+    transparent: false
+}
+
+export default Table
