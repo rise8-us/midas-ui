7efe6389b77dfab24e432133e4913b2971ec4056
David Lamberson <dlamberson@rise8.us>
2021-04-08 11:02:34 -0400

    handles any hex color

--- src/Components/ColorPicker/ColorPicker.js
+++ src/Components/ColorPicker/ColorPicker.js
@@ -1,34 +1,54 @@
 import { Box, TextField } from '@material-ui/core'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { CirclePicker } from 'react-color'
 
-function ColorPicker({ onColorPickerChange, color, colorError }) {
+function ColorPicker({ onChange, errors, initialColor }) {
+
+    const [color, setColor] = useState(initialColor)
+
+    const onColorPickerChange = (colorPicked) => setColor(colorPicked.hex)
+    const onColorInputChange = (e) => {
+        e.target.value.match(/^#[0-9A-Fa-f]{0,6}$/) && setColor(e.target.value)
+    }
+
+    const validHex = (hex) => hex.toUpperCase().match(/^#[0-9A-F]{6}$/)
+
+    useEffect(() => {
+        if (validHex(color)) onChange(color)
+        else (onChange(undefined))
+    }, [color])
+
     return (
-        <div>
+        <>
             <TextField
-                label = 'Select Color'
+                label = 'Color'
                 data-testid = 'ColorPicker__input-color'
                 value = {color}
-                error = { colorError.length > 0 }
-                helperText = { colorError[0] ?? '' }
+                error = { errors.length > 0 }
+                helperText = { errors[0] ?? '' }
+                onChange = {onColorInputChange}
                 margin = 'dense'
-                disabled = {false}
-                fullWidth
             />
             <Box display = 'flex' justifyContent = 'space-around' style = {{ paddingTop: '20px' }}>
                 <CirclePicker
-                    color = {color}
-                    onChange = {onColorPickerChange} />
+                    color = {validHex(color) ? color : undefined}
+                    onChange = {onColorPickerChange}
+                />
             </Box>
-        </div>
+        </>
     )
 }
 
 export default ColorPicker
 
 ColorPicker.propTypes = {
-    onColorPickerChange: PropTypes.func.isRequired,
-    color: PropTypes.string.isRequired,
-    colorError: PropTypes.array.isRequired
+    onChange: PropTypes.func.isRequired,
+    errors: PropTypes.array,
+    initialColor: PropTypes.string
+}
+
+ColorPicker.defaultProps = {
+    errors: [],
+    initialColor: '#'
 }
