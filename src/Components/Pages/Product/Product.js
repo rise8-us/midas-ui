bf037bc7b462b033b6f957cc3bf60cc2b9d1aee5
Avery Merriex <averymerriex@gmail.com>
2021-04-26 20:28:55 +0000

    Resolve "Improve Header on Product Page"

--- src/Components/Pages/Product/Product.js
+++ src/Components/Pages/Product/Product.js
@@ -1,22 +1,133 @@
-import { Box, Typography } from '@material-ui/core'
-import React from 'react'
-import { useSelector } from 'react-redux'
+import { Box, makeStyles, TextField, IconButton } from '@material-ui/core'
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { selectProductById } from '../../../Redux/Products/selectors'
 import { getUrlParam } from '../../../Utilities/queryParams'
 import { Page } from '../../Page'
+import { requestUpdateProduct } from '../../../Redux/Products/actions'
+import { Tag } from '../../../Components/Tag'
+import { Edit } from '@material-ui/icons'
+import SaveOutlinedIcon from '@material-ui/icons/SaveOutlined'
+
+const useStyles = makeStyles((theme) => ({
+    box: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    row: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: '36px',
+        paddingBottom: '10px'
+    },
+    disabled: {
+        color: theme.palette.text.primary
+    },
+    notDisabledh6: {
+        fontFamily: theme.typography.h6.fontFamily,
+        fontSize: theme.typography.h6.fontSize,
+        fontWeight: theme.typography.h6.fontWeight,
+        lineHeight: theme.typography.h6.lineHeight,
+        color: theme.palette.text.secondary
+    },
+    notDisabledh4: {
+        fontFamily: theme.typography.h4.fontFamily,
+        fontSize: theme.typography.h4.fontSize,
+        fontWeight: theme.typography.h4.fontWeight,
+        lineHeight: theme.typography.h4.lineHeight
+    },
+    icon: {
+        height: 42
+    }
+}))
 
 function Product() {
+    const classes = useStyles()
+    const dispatch = useDispatch()
 
     const id = parseInt(getUrlParam('products'))
 
     const product = useSelector(state => selectProductById(state, id))
 
+    const [isDisabled, setDisabled] = useState(true)
+    const [visionStatement, setVision] = useState(product.visionStatement)
+    const [name, setName] = useState(product.name)
+    const [loaded, setLoaded] = useState(false)
+
+    const onVisionChange = (e) => !isDisabled && setVision(e.target.value)
+    const onNameChange = (e) => !isDisabled && setName(e.target.value)
+
+    useEffect(() => {
+        if (!loaded && product.id === id) {
+            setVision(product.visionStatement)
+            setName(product.name)
+            setLoaded(true)
+        }
+    })
+
+    const onSubmit = () => {
+        setName(name)
+        setVision(visionStatement)
+        dispatch(requestUpdateProduct({
+            ...product,
+            name,
+            visionStatement,
+        }))
+        setDisabled(!isDisabled)
+    }
+
     return (
-        <Page>
-            <Box display = 'flex' flexDirection = 'column' padding = '36px'>
-                <Typography variant = 'h5'>Hello {product.name}</Typography>
-                <Typography>Vision: {product.visionStatement}</Typography>
-                <Typography>Problem Statement: {product.problemStatement}</Typography>
+        <Page >
+            <Box className = {classes.row}>
+                <Box className = {classes.box} >
+                    <TextField
+                        disabled = {isDisabled}
+                        InputProps = {{
+                            disableUnderline: isDisabled,
+                            autoFocus: true,
+                            'data-testid': 'Product__input-name',
+                            className: classes.notDisabledh4,
+                            classes: {
+                                disabled: classes.disabled
+                            }
+                        }}
+                        value = {name}
+                        onChange = {onNameChange}
+                    />
+                    <TextField
+                        disabled = {isDisabled}
+                        InputProps = {{
+                            disableUnderline: isDisabled,
+                            'data-testid': 'Product__input-vision',
+                            className: classes.notDisabledh6
+                        }}
+                        value = {visionStatement}
+                        onChange = {onVisionChange}
+                        multiline
+                    />
+                </Box>
+                {isDisabled ?
+                    <IconButton
+                        className = {classes.icon}
+                        data-testid = 'SaveAlt__icon'
+                        onClick = {() => setDisabled(!isDisabled)}>
+                        <Edit />
+                    </IconButton> :
+                    <IconButton
+                        className = {classes.icon}
+                        data-testid = 'SaveOut__icon'
+                        color = 'secondary'
+                        onClick = {onSubmit}
+                    >
+                        <SaveOutlinedIcon />
+                    </IconButton>
+                }
+            </Box>
+            <Box display = 'flex' alignItems = 'right' paddingLeft = '30px'>
+                {product.tags.map((tag, index) => (
+                    <Tag { ...tag } key = {index}/>
+                ))}
             </Box>
         </Page>
     )
--- src/Components/Pages/Product/Product.test.js
+++ src/Components/Pages/Product/Product.test.js
@@ -1,28 +1,81 @@
 import React from 'react'
-import { render, screen, useDispatchMock, useModuleMock } from '../../../Utilities/test-utils'
+import {
+    fireEvent, render, screen, useDispatchMock, useModuleMock, userEvent
+} from '../../../Utilities/test-utils'
 import { Product } from './index'
 
 describe('<Product>', () => {
 
     const selectProductByIdMock = useModuleMock('Redux/Products/selectors', 'selectProductById')
+    const submitProductMock = useModuleMock('Redux/Products/actions', 'requestUpdateProduct')
+    const queryParamsMock = useModuleMock('Utilities/queryParams', 'getUrlParam')
 
     const product = {
         id: 0,
         name: 'Product 1',
         visionStatement: 'great vision',
         problemStatement: 'great problem',
+        tagIds: [4],
+        tags: [
+            {   id: 4,
+                label: 'Some tags',
+                description: null,
+                color: ''
+            }
+        ]
+
     }
 
     beforeEach(() => {
         useDispatchMock().mockReturnValue({})
+        queryParamsMock.mockReturnValue(0)
         selectProductByIdMock.mockReturnValue(product)
     })
 
     test('Has correct text', () => {
         render(<Product />)
-        expect(screen.getByText('Hello Product 1')).toBeInTheDocument()
-        expect(screen.getByText('Vision: great vision')).toBeInTheDocument()
-        expect(screen.getByText('Problem Statement: great problem')).toBeInTheDocument()
+        expect(screen.getByTestId('Product__input-name').querySelector('input')).toHaveValue('Product 1')
+        expect(screen.getByText('great vision')).toBeInTheDocument()
+        expect(screen.getByText('Some tags')).toBeInTheDocument()
     })
+
+    test('should have empty text', () => {
+        selectProductByIdMock.mockReturnValue({
+            name: '',
+            visionStatement: '',
+            problemStatement: '',
+            tags: [],
+            projects: []
+        })
+        render(<Product />)
+
+        fireEvent.click(screen.getByTestId('SaveAlt__icon'))
+        expect(screen.getByTestId('Product__input-name').querySelector('input')).toHaveValue('')
+    })
+
+    test('should call onSubmit for update product vision', () => {
+        const { rerender } = render(<Product />)
+        fireEvent.click(screen.getByTestId('SaveAlt__icon'))
+
+        rerender(<Product />)
+
+        const name = 'My Edited Product'
+        const visionStatement = 'New visionStatement'
+
+        const nameInput = screen.getByTestId('Product__input-name').querySelector('input')
+        const visionStatementInput = screen.getByTestId('Product__input-vision').querySelector('textarea')
+        userEvent.clear(nameInput)
+        userEvent.clear(visionStatementInput)
+
+        userEvent.type(visionStatementInput, visionStatement)
+        userEvent.type(nameInput, name)
+
+        fireEvent.click(screen.getByTestId('SaveOut__icon'))
+
+        expect(submitProductMock).toHaveBeenCalledWith({
+            ...product, name, visionStatement
+        })
+    })
+
 
 })
