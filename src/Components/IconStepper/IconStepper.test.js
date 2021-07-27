c651271912da035d29ee3a5568abb8a77a8aafe2
David Lamberson <dlamberson@rise8.us>
2021-07-27 22:07:56 +0000

    Resolve "Link to JIRA Tracking KanBan"

--- src/Components/IconStepper/IconStepper.test.js
+++ src/Components/IconStepper/IconStepper.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '../../Utilities/test-utils'
+import { fireEvent, render, screen, waitFor } from '../../Utilities/test-utils'
 import { IconStepper } from './index'
 
 describe('<IconStepper />', () => {
@@ -7,5 +7,12 @@ describe('<IconStepper />', () => {
         render(<IconStepper steps = {['test']} currentStep = {0}/>)
 
         expect(screen.getByText('test')).toBeInTheDocument()
+    })
+
+    test('should display tooltip with jira info', () => {
+        render(<IconStepper steps = {['Jira blah boop']} currentStep = {0}/>)
+
+        fireEvent.mouseOver(screen.getByText('Jira blah boop'))
+        waitFor(() => { expect(screen.getByText('Jira Kanban board')).toBeInTheDocument() })
     })
 })
