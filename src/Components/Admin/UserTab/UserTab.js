7920522babc79237bc23799fbe36f60b3cd0fac9
David Lamberson <dlamberson@rise8.us>
2022-06-01 12:44:34 +0000

    Resolve "Admin - Select User Modal"

--- src/Components/Admin/UserTab/UserTab.js
+++ src/Components/Admin/UserTab/UserTab.js
@@ -1,37 +1,29 @@
 import { Search } from '@mui/icons-material'
-import { Box } from '@mui/material'
+import { Stack } from '@mui/material'
 import { SearchUsers } from 'Components/Search'
 import { Table } from 'Components/Table'
-import { UserRoles } from 'Components/UserRoles'
-import { UserSettings } from 'Components/UserSettings'
-import { useCallback, useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-import { selectUserById } from 'Redux/Users/selectors'
+import { useCallback, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { openPopup } from 'Redux/Popups/actions'
+import userConstants from 'Redux/Users/constants'
 import { styled } from 'Styles/materialThemes'
 
-const StyledDiv = styled('div')(({ theme }) => ({
-    margin: theme.spacing(1, 3)
-}))
-
 const StyledSearchUsers = styled(SearchUsers)(({ theme }) => ({
+    position: 'sticky',
+    top: '68px',
+    boxShadow: '0 0 10px black',
     height: 48,
-    margin: theme.spacing(3, 3, 1, 3),
-    borderRadius: 6,
+    borderRadius: theme.spacing(1),
     paddingLeft: theme.spacing(1),
     paddingRight: theme.spacing(1),
     backgroundColor: theme.palette.background.paper
 }))
 
-const isValidUser = (userId) => {
-    return typeof userId === 'number' ? true : false
-}
-
 function UserTab() {
-    const [results, setResults] = useState([])
-    const [userId, setUserId] = useState()
-    const [show, setShow] = useState(false)
+    const dispatch = useDispatch()
 
-    const user = useSelector((state) => selectUserById(state, userId))
+    const [results, setResults] = useState([])
+
     const buildRows = useCallback(() => {
         return results.map((r) => ({
             data: [r.id, r.username, r.email, r.displayName],
@@ -41,17 +33,12 @@ function UserTab() {
         }))
     }, [results])
 
-    const showUser = (data) => {
-        setShow(prev => !prev)
-        setUserId(data[0])
+    const updateUser = (data) => {
+        dispatch(openPopup(userConstants.UPDATE_USER, 'UserPopup', { id: data[0] }))
     }
 
-    useEffect(() => {
-        setShow(isValidUser(userId))
-    }, [userId])
-
     return (
-        <Box display = 'flex' flexDirection = 'column'>
+        <Stack spacing = {1} m = {t => t.spacing(9, 3, 1, 3)}>
             <StyledSearchUsers
                 onDataReturn = {data => setResults(data ?? [])}
                 title = ''
@@ -72,23 +59,13 @@ function UserTab() {
                     }}
                 />}
             />
-            <>
-                <StyledDiv>
-                    <Table
-                        columns = {['id', 'username', 'email', 'display name']}
-                        rows = {buildRows()}
-                        onRowClick = {showUser}
-                        tableWidth = '100%'
-                    />
-                </StyledDiv>
-                {show && (
-                    <>
-                        <UserSettings id = {userId} />
-                        <UserRoles user = {user} editable />
-                    </>
-                )}
-            </>
-        </Box>
+            <Table
+                columns = {['id', 'username', 'email', 'display name']}
+                rows = {buildRows()}
+                onRowClick = {updateUser}
+                tableWidth = '100%'
+            />
+        </Stack>
     )
 }
 
