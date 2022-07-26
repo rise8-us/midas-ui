eee239ea0134d0d2f9d448e70953cd2a20a7e7c6
dustintktran <dtran@rise8.us>
2022-07-26 10:15:59 -0700

    CSS Update

--- src/Components/FileManager/FileManager.js
+++ src/Components/FileManager/FileManager.js
@@ -1,6 +1,7 @@
+import { Download } from '@mui/icons-material'
 import { Button, Stack, TextField, Typography } from '@mui/material'
 import PropTypes from 'prop-types'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { requestGetFile, requestGetFileNames, requestSaveFile } from 'Redux/FileManager/actions'
 import { selectPortfolioById } from 'Redux/Portfolios/selectors'
@@ -9,10 +10,13 @@ import { selectProductById } from 'Redux/Products/selectors'
 export default function FileManager({ id }) {
     const dispatch = useDispatch()
 
+    const inputRef = useRef(null)
+
     const product = useSelector(state => selectProductById(state, id))
     const portfolio = useSelector(state => selectPortfolioById(state, product?.portfolioId))
 
     const [file, setFile] = useState(undefined)
+    const [fileName, setFileName ] = useState('')
     const [allFileNames, setAllFileNames] = useState([])
 
     const getAllFiles = () => {
@@ -21,14 +25,15 @@ export default function FileManager({ id }) {
         })
     }
 
-    const handleDownloadFile = (fileName) => {
-        dispatch(requestGetFile({ fileName: fileName.substring(fileName.lastIndexOf('/') + 1), filePath: fileName }))
+    const handleDownloadFile = (filePath) => {
+        dispatch(requestGetFile({ fileName: filePath.substring(filePath.lastIndexOf('/') + 1), filePath: filePath }))
     }
 
     const handleChange = (e) => {
         const formData = new FormData()
         formData.append('file', e.target.files[0])
         setFile(formData)
+        setFileName(e.target.files[0].name)
     }
 
     const handleSave = () => {
@@ -49,34 +54,45 @@ export default function FileManager({ id }) {
         getAllFiles()
     }, [])
 
-
     return (
-        <Stack direction = 'row' justifyContent = 'space-between'>
+        <Stack direction = 'row'>
             <Stack direction = 'column'>
                 <TextField
-                    type = 'file'
-                    onChange = {handleChange}
+                    autoFocus
+                    fullWidth
+                    label = {fileName.length > 0 ? '' : 'Choose File'}
+                    variant = 'outlined'
+                    placeholder = 'Choose File'
+                    sx = {{ marginY: '16px' }}
+                    onClick = {() => inputRef.current.click()}
+                    value = {fileName}
                 />
+                <input type = 'file' hidden ref = {inputRef} onChange = {handleChange}/>
                 <Button variant = 'outlined' onClick = {handleSave}>
-                    Save File
+                    Upload
                 </Button>
             </Stack>
-            <Stack>
+            <Stack marginLeft = {4}>
+                <Typography variant = 'h5' marginY = {2}>Existing File Uploads from {product.name}</Typography>
                 {allFileNames?.map((filePath, index) =>{
                     return (
-                        <Typography
+                        <Stack
+                            direction = 'row'
                             key = {index}
+                            onClick = {() => handleDownloadFile(filePath)}
                             sx = {{
+                                fontSize: '18px',
                                 cursor: 'pointer',
                                 '&:hover': {
                                     color: 'primary.main',
                                     textDecoration: 'underline',
                                 },
-                            }}
-                            onClick = {() => handleDownloadFile(filePath)}
-                        >
-                            {filePath.substring(filePath.lastIndexOf('/') + 1)}
-                        </Typography>
+                            }}>
+                            <Download />
+                            <Typography>
+                                {filePath.substring(filePath.lastIndexOf('/') + 1)}
+                            </Typography>
+                        </Stack>
                     )
                 }
                 )}
