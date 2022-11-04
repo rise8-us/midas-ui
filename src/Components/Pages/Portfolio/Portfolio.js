cf25303de9db830178df83285fe3f7ac6e6608ed
Kira Ivarsson <kivarsson@rise8.us>
2022-11-04 09:30:12 -0400

    Remove gantt notes section

--- src/Components/Pages/Portfolio/Portfolio.js
+++ src/Components/Pages/Portfolio/Portfolio.js
@@ -11,7 +11,6 @@ import {
     Tooltip,
     Typography
 } from '@mui/material'
-import { GanttPortfolioNote } from 'Components/Gantt/GanttPortfolioNote'
 import { Page } from 'Components/Page'
 import { PageMetrics } from 'Components/Tabs/PageMetrics'
 import * as PortfolioTab from 'Components/Tabs/PortfolioPage'
@@ -121,7 +120,6 @@ export default function Portfolio() {
                 <Box paddingY = {3}>
                     {(portfolioTab === 'roadmap' || portfolioTab === undefined) &&
                         <Suspense fallback = {<div data-testid = 'Portfolio__fallback'/>}>
-                            <GanttPortfolioNote id = {id}/>
                             <PortfolioTab.PortfolioRoadmap portfolioId = {id}/>
                         </Suspense>
                     }
